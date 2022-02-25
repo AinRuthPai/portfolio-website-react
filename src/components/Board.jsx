4fd27d0d47a11ceb8e986e0a7e5d9e983bb1bfd2
AinRuthPai <tanbing@naver.com>
2022-02-25 21:54:57 +0900

    게시판 수정

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,33 +1,30 @@
-import { ListGroup, Badge, Button } from "react-bootstrap";
-import { useState } from "react";
+import { Table, Button } from "react-bootstrap";
 
 function Board() {
-  const [postTitle, setPostTitle] = useState(["html", "css", "js"]);
-
   return (
     <>
-      {postTitle.map((post, i) => {
-        return (
-          <div className='board'>
-            <ListGroup as='ul'>
-              <ListGroup.Item as='li' className='d-flex justify-content-between align-items-start'>
-                <div className='ms-2 me-auto'>
-                  <div className='fw-bold' key={i}>
-                    {post}
-                  </div>
-                  Cras justo odioooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
-                </div>
-                <Badge variant='primary' pill>
-                  14
-                </Badge>
-              </ListGroup.Item>
-            </ListGroup>
-          </div>
-        );
-      })}
+      <Table striped bordered hover className='board'>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>글제목</th>
+            <th>작성일자</th>
+            <th>작성자</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+          </tr>
+        </tbody>
+      </Table>
+
       <Button variant='primary' className='boardWriteBtn'>
         글쓰기
-      </Button>{" "}
+      </Button>
     </>
   );
 }
